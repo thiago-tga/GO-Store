4eb234e2edf05cfcc7c12c8e80acd855f2cd02e2
mainara07 <assis.mainara@gmail.com>
2021-03-21 22:18:45 -0300

    feature: finalizando o footer

--- src/@vtex/gatsby-theme-store/components/Footer/SocialMedias.tsx
+++ src/@vtex/gatsby-theme-store/components/Footer/SocialMedias.tsx
@@ -1,11 +1,19 @@
-import { Flex, Grid, Link, Image, Text } from '@vtex/store-ui'
+import { Flex, Link, Image, Text, Heading } from '@vtex/store-ui'
 import React from 'react'
 import type { FC } from 'react'
 
 const SocialMedias: FC<{ variant?: string }> = ({ variant }) => (
-  <Grid gap={2} columns={[3]}>
-    <Flex variant={variant}>
-      <Text>
+  <Flex variant={variant}>
+    <Flex
+      sx={{
+        width: 540,
+        height: 28,
+        position: 'absolute',
+        marginLeft: 85,
+        // left: 72,
+      }}
+    >
+      <Text sx={{ color: '#3F3F40', fontSize: '12px', textAlign: 'left' }}>
         Todas as regras e promoções são válidas apenas para produtos vendidos e
         entregues pela nossa Loja. O valor de oferta do produto será garantido
         após a finalização da compra. Havendo divergência, prevalecerá o menor
@@ -13,22 +21,31 @@ const SocialMedias: FC<{ variant?: string }> = ({ variant }) => (
       </Text>
     </Flex>
 
-    <Flex sx={{ marginLeft: '200px' }}>
+    <Flex
+      sx={{
+        width: 150,
+        height: 16,
+        position: 'absolute',
+        marginLeft: 800,
+        // left: 70,
+        gap: 3,
+      }}
+    >
       <Link
         href="https://www.facebook.com/vtexonline/"
         target="_blank"
         rel="noreferrer"
-        sx={{
-          height: '48px',
-          width: '48px',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+        // sx={{
+        //   height: '48px',
+        //   width: '48px',
+        //   alignItems: 'center',
+        //   justifyContent: 'center',
+        // }}
       >
         <Image
-          sx={{ height: 32, width: 32, mr: 1 }}
-          height="32px"
-          width="32px"
+          // sx={{ height: 32, width: 32, mr: 1 }}
+          height="48px"
+          width="48px"
           loading="lazy"
           alt="Facebook"
           src="https://storecomponents.vtexassets.com/_v/public/assets/v1/published/vtex.store-footer@2.20.1/public/react/ebd4bd10e66138168eb6a582e00790ea.svg"
@@ -38,15 +55,15 @@ const SocialMedias: FC<{ variant?: string }> = ({ variant }) => (
         href="https://www.instagram.com/vtextruecloud/"
         target="_blank"
         rel="noreferrer"
-        sx={{
-          height: '48px',
-          width: '48px',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+        // sx={{
+        //   height: '48px',
+        //   width: '48px',
+        //   alignItems: 'center',
+        //   justifyContent: 'center',
+        // }}
       >
         <Image
-          sx={{ height: 32, width: 32, mr: 1 }}
+          // sx={{ height: 32, width: 32, mr: 1 }}
           height="48px"
           width="48px"
           loading="lazy"
@@ -54,21 +71,22 @@ const SocialMedias: FC<{ variant?: string }> = ({ variant }) => (
           src="https://storecomponents.vtexassets.com/_v/public/assets/v1/published/vtex.store-footer@2.20.1/public/react/6a61a302319c062aceb9562a66381a63.svg"
         />
       </Link>
+
       <Link
         href="https://twitter.com/vtexonline"
         target="_blank"
         rel="noreferrer"
-        sx={{
-          height: '48px',
-          width: '48px',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+        // sx={{
+        //   height: '48px',
+        //   width: '48px',
+        //   alignItems: 'center',
+        //   justifyContent: 'center',
+        // }}
       >
         <Image
-          sx={{ height: 32, width: 32, mr: 1 }}
-          height="32px"
-          width="32px"
+          // sx={{ height: 32, width: 32, mr: 1 }}
+          height="48px"
+          width="48px"
           loading="lazy"
           alt="Twitter"
           src="https://storecomponents.vtexassets.com/_v/public/assets/v1/published/vtex.store-footer@2.20.1/public/react/5f12d2a63f43d3a243550ff6400b4870.svg"
@@ -78,25 +96,70 @@ const SocialMedias: FC<{ variant?: string }> = ({ variant }) => (
         href="https://www.youtube.com/user/VTEXTV"
         target="_blank"
         rel="noreferrer"
-        sx={{
-          height: '48px',
-          width: '48px',
-          alignItems: 'center',
-          justifyContent: 'center',
-          // hehehe perdeu o footer
-        }}
+        // sx={{
+        //   height: '48px',
+        //   width: '48px',
+        //   alignItems: 'center',
+        //   justifyContent: 'center',
+        // }}
       >
         <Image
-          sx={{ height: 32, width: 32 }}
-          height="32px"
-          width="32px"
+          height="48px"
+          width="48px"
           loading="lazy"
           alt="Youtube"
           src="https://storecomponents.vtexassets.com/_v/public/assets/v1/published/vtex.store-footer@2.20.1/public/react/20a214b4866fd6d42a6dfed070c0057b.svg"
         />
       </Link>
     </Flex>
-  </Grid>
+    <Flex
+      sx={{
+        marginLeft: 1200,
+        marginRight: 86,
+        flexDirection: 'column',
+        position: 'absolute',
+        width: '95px',
+        height: '51,67px',
+        // left: 864,
+        // top: 470,
+      }}
+    >
+      <Flex
+        sx={{
+          position: 'absolute',
+          width: '110px',
+          height: '14px',
+          right: '10px',
+          bottom: '50px',
+        }}
+      >
+        <Heading
+          as="h3"
+          sx={{
+            color: '#979899',
+            fontSize: '14px',
+            lineHeight: '14px',
+            fontWeight: 'normal',
+            // marginBottom: '20px',
+            display: 'flex',
+            alignItems: 'center',
+            textAlign: 'center',
+            letterSpacing: '0.1em',
+            textTransform: 'uppercase',
+          }}
+        >
+          Powered by
+        </Heading>
+      </Flex>
+      <Image
+        height="140px"
+        width="140px"
+        loading="lazy"
+        alt="logo_vtex"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/VTEX_Logo.svg/1280px-VTEX_Logo.svg.png"
+      />
+    </Flex>
+  </Flex>
 )
 
 export default SocialMedias
